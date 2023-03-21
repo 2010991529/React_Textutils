76ee587533450d52f5df1691a008a6d409da7063
2010991529 <devanshi1529.be20@chitkara.edu.in>
2023-03-22 00:53:15 +0530

    Capitilizing whole text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 //import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
-
+import Textbox from './Components/Textbox'
 function App() {
   return (
     <>
 <Navbar title="TextUtils" about = "About TextUtils"/>
+<Textbox heading="Add views below..."/>
     </>
   );
 }
 
 export default App;
+ 
