26d46ddb447ee081b3e2f2dbf3222f77ba156ea6
2010991529 <devanshi1529.be20@chitkara.edu.in>
2023-03-22 01:07:36 +0530

    Word and time Counter and preview text

--- src/Components/Textbox.js
+++ src/Components/Textbox.js
@@ -7,6 +7,11 @@ export default function TextForm(props){
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleLoClick = () =>{
+        console.log("Lowercase was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
     const handleOnChange = (event) =>{
         console.log("On change");
         setText(event.target.value);
@@ -16,12 +21,22 @@ export default function TextForm(props){
     //text = "new text";//Wrong way to change state
     //setText("new Text");//Right way to change the state
     return(
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
         </div>
+        <div className='container'>
+            <h1>Your text summary</h1>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
